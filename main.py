25e981e050f6fa9c0e7faa5936b11bf09f461004
Avellar19 <jpavellar2004@gmail.com>
2022-11-03 09:57:54 -0300

    update

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+#codigo usado para a vizualizaçao das informaçoes presentes no banco de dados
 import sqlite3
 
 conn = sqlite3.connect('empresa.db') 
 c = conn.cursor()
 
-j = c.execute("SELECT * FROM empregados").fetchall()
+v = input('Diga o cpf de quem deseja visualizar os dados: ')
+
+j = c.execute("SELECT * FROM empregados where CPF = '"+v+"'").fetchall()
                      
 conn.commit()
 
